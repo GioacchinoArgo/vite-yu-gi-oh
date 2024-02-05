646a29b669bbac82232f5c575756183bcd02d6c8
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-02-05 21:29:48 +0100

    added header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header class="d-flex justify-content-center align-items-center shadow-lg">
+        <h1 class="text-uppercase fw-bold">pokedex</h1>
+        <div class="circle"></div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    background-color: #e62117;
+    padding: 35px;
+    color: white;
+    font-size: 32px;
+    border-bottom: 10px solid #333333;
+    position: relative;
+
+    .circle {
+        height: 55px;
+        width: 55px;
+        border-radius: 50%;
+        background-color: #FFFFFF;
+        border: 10px solid #333333;
+        position: absolute;
+        bottom: -30px;
+        left: 48.2%;
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
